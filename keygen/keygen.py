d12adaf25a7fd3a8552dd18578acdddf1d287b53
Bobsans <mr.bobsans@gmail.com>
2018-03-22 18:27:55 +0300

    Add checks for validate input arguments

--- keygen/keygen.py
+++ keygen/keygen.py
@@ -7,9 +7,14 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 def main():
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument('-s', '--symbols', dest='symbols', default='uldp', type=str, help='Symbols can set of next vals:\n  u - ASCII uppercase letters\n  l - ASCII lowеrcase letters\n  d - digits\n  p - punctuation\n  h - HEX digits\n  o - OCT digits\n  b - BIN digits\nDefault: uldp')
-    parser.add_argument('-l', '--length', dest='length', default=32, type=int, help='Length of result string\nDefault: 32')
+    parser.add_argument('-l', '--length', dest='length', default=32, type=int, help='Length of generated key\nDefault: 32')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Output more info\nDefault: false')
     args = parser.parse_args()
+
+    invalid = ''.join(filter(lambda s: s not in 'uldphob', args.symbols))
+    if invalid:
+        sys.stderr.write(f'Invalid symbol set identifier: "{invalid}".\n')
+        exit()
 
     chars = ''
     if 'u' in args.symbols:
@@ -27,7 +32,9 @@ def main():
     if 'b' in args.symbols:
         chars += '01'
 
-    result = ''.join([random.choice(chars) for _ in range(args.length)])
+    charset = list(set(chars))
+
+    result = ''.join([random.choice(charset) for _ in range(args.length)])
 
     if args.verbose:
         sys.stdout.write(f'Symbols: {chars}\n')
